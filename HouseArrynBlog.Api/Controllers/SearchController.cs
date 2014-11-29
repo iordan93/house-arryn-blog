86165b60f0fdbfdab35e6f11a58e634a237385e2
Yordan Darakchiev <iordan93@gmail.com>
2014-11-29 20:13:47 +0200

    Added the functionality to search by tags

---
+++ HouseArrynBlog.Api/Controllers/SearchController.cs
@@ -0,0 +1,33 @@
+﻿using HouseArrynBlog.Api.Models;
+using HouseArrynBlog.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HouseArrynBlog.Api.Controllers
+{
+    [RoutePrefix("api/Search")]
+    public class SearchController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Search([FromUri]string query)
+        {
+            var context = new HouseArrynBlogContext();
+            var result = new SearchResultViewModel() { Query = query, Posts = new List<ConcisePostViewModel>() };
+            var tag = context.Tags.Where(t => t.Name == query).FirstOrDefault();
+            if (tag != null)
+            {
+                var posts = tag.Posts
+                    .AsQueryable()
+                    .OrderByDescending(p => p.PublishDate)
+                    .Select(ConcisePostViewModel.FromPost);
+                result.Posts = posts;
+            }
+
+            return Json(result);
+        }
+    }
+}
---
+++ HouseArrynBlog.Api/Models/SearchResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HouseArrynBlog.Api.Models
+{
+    public class SearchResultViewModel
+    {
+        public string Query { get; set; }
+
+        public IEnumerable<ConcisePostViewModel> Posts { get; set; }
+    }
+}
