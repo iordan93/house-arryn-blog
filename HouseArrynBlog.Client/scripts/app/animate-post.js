dc83f5307e135b496d8a45e1b2f8ebeeb640fdfd
DimitarRuskov <mitko_ruskov@abv.bg>
2014-12-02 20:10:46 +0200

    #7 shrinking and extending post elements

--- HouseArrynBlog.Client/scripts/app/animate-post.js
+++ HouseArrynBlog.Client/scripts/app/animate-post.js
@@ -5,4 +5,7 @@
     $(document).on("click", ".view-comments", function (event) {
         $(this).closest(".postContent").children(".post-comments").slideDown(300);
     });
+    $(document).on("click", ".view-all", function (event) {
+        $(this).closest(".postContent").children("p").css( "height", "auto");
+    });
 }());
