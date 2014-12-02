37ef06f341021737db2936254c92ef47f10112d0
Yordan Darakchiev <iordan93@gmail.com>
2014-12-02 20:59:06 +0200

    Updated the CategoriesController file

---
+++ HouseArrynBlog.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using HouseArrynBlog.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HouseArrynBlog.Api.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetAllCategories() 
+        {
+            var context = new HouseArrynBlogContext();
+            var categories = context.Categories
+                .Select(c => new
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                });
+            return Json(categories);
+        }
+    }
+}
