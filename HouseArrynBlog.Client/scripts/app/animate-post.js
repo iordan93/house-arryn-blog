0a8eee5934ee45cb269c3401a42a2d57773a4c2f
DimitarRuskov <mitko_ruskov@abv.bg>
2014-12-04 12:54:48 +0200

    #17 rewire post listing and comment form

--- HouseArrynBlog.Client/scripts/app/animate-post.js
+++ HouseArrynBlog.Client/scripts/app/animate-post.js
@@ -1,15 +1,17 @@
 ﻿(function () {
-    $(document).on("click", ".add-comment-button", function (event) {
-        $(this).closest(".postContent").children(".comment-form").slideDown(300);
-    });
-    $(document).on("click", ".view-comments", function (event) {
+    
+   /* $(document).on("click", ".view-comments", function (event) {
         $(this).closest(".postContent").children(".post-comments").slideDown(300);
     });
     $(document).on("click", ".view-all", function (event) {
         $(this).closest(".postContent").children("p").css( "height", "auto");
-    });
+    });*/
 
     $(document).on('mouseenter mouseleave', '.menu', function () {
         $(this).toggleClass('hover');
+    });
+
+    $(document).on('click', '#add-comment', function () {
+        $('.comment-form').toggleClass('hidden');
     });
 }());
