e0550192b8eddbe48bdeece6bd7d9aa41f0a2f53
Yordan Darakchiev <iordan93@gmail.com>
2014-12-08 04:44:20 +0200

    #13 Admin login / logout now fully working

--- HouseArrynBlog.Client/admin/scripts/admin-controller.js
+++ HouseArrynBlog.Client/admin/scripts/admin-controller.js
@@ -5,20 +5,27 @@
         init: function () {
             this.repository = repository.get(rootUrl);
         },
-
+        displayMessage: function (type, text) {
+            $("<div>")
+                .addClass("alert alert-" + type)
+                .text(text)
+                .appendTo(("#messages"))
+                .fadeOut(4000);
+        },
         loadMainPage: function () {
             var self = this;
+            debugger;
             if (this.repository.account.isLoggedIn()) {
-                console.log($("#nav-right"));
-
-                $("#nav-right").append($("<li>").html("<form class=\"navbar-form navbar-left\"><button id=\"logout-button\" class=\"btn btn-default\">Logout</button></forn>"));
+                $("#nav-right").html($("<li>").html("<form class=\"navbar-form navbar-left\"><button id=\"logout-button\" class=\"btn btn-default\">Logout</button></forn>"));
                 $("#logout-button").click(function (e) {
                     self.repository.account.logout()
                     .then(function (data) {
-                        console.log(    'fa');
+                        $("#logout-button").hide();
+                        self.displayMessage("success", "You have logged out successfully.");
                         self.loadMainPage();
-                    }, function (err) { 
-                        console.log(err); });
+                    }, function (err) {
+                        console.log(err);
+                    });
                     e.preventDefault();
                 });
             } else {
@@ -32,8 +39,10 @@
                         self.repository.account.login(username, password)
                         .then(function (data) {
                             $("#login-form").hide();
+                            self.displayMessage("success", "You have logged in successfully.");
                             self.loadMainPage();
                         }, function (err) {
+                            self.displayMessage("danger", "Invalid username and / or password.");
                             console.log(err);
                         });
                         e.preventDefault();
