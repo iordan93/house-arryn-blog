808c24004bb37371c5f181d357e073595f777221
Yordan Darakchiev <iordan93@gmail.com>
2014-12-08 11:27:35 +0200

    Added the functionality of creating a new post

--- HouseArrynBlog.Client/admin/scripts/admin-controller.js
+++ HouseArrynBlog.Client/admin/scripts/admin-controller.js
@@ -14,13 +14,22 @@
         },
         loadMainPage: function () {
             var self = this;
-            debugger;
-            if (this.repository.account.isLoggedIn()) {
+            if (isLoggedIn()) {
                 $("#nav-right").html($("<li>").html("<form class=\"navbar-form navbar-left\"><button id=\"logout-button\" class=\"btn btn-default\">Logout</button></forn>"));
+                $("#nav-left").show();
+
+                $("#menu-add-post").click(function () {
+                    self.loadCreateForm();
+                });
+                $("#menu-edit-post").click(function () {
+                    self.loadEditForm();
+                });
+
                 $("#logout-button").click(function (e) {
                     self.repository.account.logout()
                     .then(function (data) {
                         $("#logout-button").hide();
+                        $("#nav-left").hide();
                         self.displayMessage("success", "You have logged out successfully.");
                         self.loadMainPage();
                     }, function (err) {
@@ -50,6 +59,54 @@
                 }, function (err) {
                     console.log(err);
                 });
+            }
+        },
+        loadCreateForm: function () {
+            var self = this;
+            ui.loadHtml("new-post-form")
+            .then(function (result) {
+                $("#main-content").html(result.templateString);
+                self.initTinyMce("#post-text");
+
+                $("#submit-post-button").click(function (e) {
+                    var title = $("#post-title").val();
+                    if (title == "") {
+                        self.displayMessage("danger", "The title is required.");
+                        return;
+                    }
+                    var content = tinyMCE.activeEditor.getContent();
+                    if (content == "") {
+                        self.displayMessage("danger", "The content is required.");
+                        return;
+                    }
+                    var tags = $("#post-tags").val().split(/,\s+/g);
+                    if (!tags || !tags[0]) {
+                        self.displayMessage("danger", "The tags are required.");
+                        return;
+                    }
+
+                    self.repository.posts.create(title, content, tags)
+                    .then(function (data) {
+                        self.displayMessage("success", "New post added successfully,");
+                        $("#new-post-form").hide();
+                        self.loadMainPage();
+                    }, function (err) {
+                        self.displayMessage("danger", "The post could not be submitted.");
+                    });
+                    e.preventDefault();
+                });
+            }, function (err) {
+                console.log(err);
+            });
+        },
+        loadEditForm: function () {
+            console.log('edit form');
+        },
+        initTinyMce: function (selector) {
+            if (typeof (tinyMCE) != "undefined") {
+                if (tinyMCE.activeEditor == null || tinyMCE.activeEditor.isHidden() != false) {
+                    tinyMCE.editors = [];
+                }
             }
         }
     });
--- HouseArrynBlog.Client/admin/scripts/admin-repository.js
+++ HouseArrynBlog.Client/admin/scripts/admin-repository.js
@@ -27,19 +27,9 @@
             // TODO: Add validation
             return httpRequester.login(username, password);
         },
-        isLoggedIn: function () {
-            return localStorage.getItem("HouseArrynBlogLogin") != null;
-        },
-        getLoginHeaders: function () {
-            if (this.isLoggedIn()) {
-                return {
-                    "Authorization": "Bearer " + JSON.parse(localStorage.getItem("HouseArrynBlogLogin")).access_token
-                }
-            }
-        },
         logout: function () {
-            if (this.isLoggedIn()) {
-                var headers = this.getLoginHeaders();
+            if (isLoggedIn()) {
+                var headers = getLoginHeaders();
                 localStorage.removeItem("HouseArrynBlogLogin");
                 return httpRequester.postJson(this.rootUrl + "logout", null, headers);
             }
@@ -65,9 +55,7 @@
                 tags: tags
             };
 
-            return httpRequester.postJson(this.rootUrl, post, {
-                "Authorization": "Bearer twFpvZFX-x4oD1HdqTpXpaBOu6CsS52XAVA_G7jee6CcrSGSRT_GnW0ES-cjTUUsPa8v635QPqocv2MYsTLojcTO1jUDEmqSEnzUdpQcX71r9bPWozYBWBNw2v9YzmmJEKw5YYlV-ojj4rzRuF56D5ZWpBy98_88oJMVczGwnpXFg5UjP57IPbNdsYiKscZYpI0UuU7OhfJQ9Hr0vvAFlO-5C3dlX4VDwgqz1TTZDWd_WvEundXJtvUGCXlebeqdTDATGhUFp3HiOvN9HViufIjKGWYoXBM_B47YSFNCyo7RHHnbY00JHcb_-f3E7fgkk1ecV9A7ZN41NZnHin4NV8uyNPTe_JVa3c7ojfyA0FdXxgeAvAXFLIAO1raAv6HZMqh863F9xEbXgrcBpZnHDPQqsNspyg0jFab7RhyaD_aiPJ_rbN03dmgpKrCHwTDgk8rKCLLUvA25Vpx4DmlqfTiL5CK4X9kl9SjWl6NyvBPNQPG7-qXYyTj2L48Wwtjc"
-            });
+            return httpRequester.postJson(this.rootUrl, post, getLoginHeaders());
         },
         list: function () {
             return httpRequester.getJson(this.rootUrl + "list");
@@ -80,3 +68,15 @@
         }
     }
 })();
+
+function isLoggedIn() {
+    return localStorage.getItem("HouseArrynBlogLogin") != null;
+}
+
+function getLoginHeaders() {
+    if (isLoggedIn()) {
+        return {
+            "Authorization": "Bearer " + JSON.parse(localStorage.getItem("HouseArrynBlogLogin")).access_token
+        };
+    }
+}
